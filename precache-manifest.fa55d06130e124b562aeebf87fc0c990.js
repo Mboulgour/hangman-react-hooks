e2f234818b84d48e07a2b3ff78b9a34e7fc122af
Mboulgour <mathieu.r.77@gmail.com>
2020-02-07 16:13:10 +0100

    Updates

---
+++ precache-manifest.fa55d06130e124b562aeebf87fc0c990.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf996ea642019a81902381f0d7475ad",
+    "url": "/hangman-react-hooks/index.html"
+  },
+  {
+    "revision": "c885d5b659f04d912380",
+    "url": "/hangman-react-hooks/static/js/2.5a3be7f0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/hangman-react-hooks/static/js/2.5a3be7f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c9c32b39ef95b72284",
+    "url": "/hangman-react-hooks/static/js/main.fa7c7c41.chunk.js"
+  },
+  {
+    "revision": "3fcf9bb846419820bd70",
+    "url": "/hangman-react-hooks/static/js/runtime-main.f9be82d0.js"
+  }
+]);
